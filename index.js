07293eebcab5df6440d600f2bd56a7a872db0e9e
Mohammad Ali Sakib <mohammad.ali.sakib.pk@gmail.com>
2021-10-29 09:17:47 +0600

    mongodb connection succesfully

--- index.js
+++ index.js
@@ -10,10 +10,29 @@ const port = process.env.PORT || 5000
 require('dotenv').config()
 app.use(cors())
 app.use(express.json())
+// database connection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rtuf5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function main(){
+    try{
+        // database client connection 
+        await client.connect()
+        console.log("database connection successfully");
+
+    }finally{
+        // perform actions on the collection object
+        // client.close();
+    }
+
+}
+main().catch(console.dir())
 
 // root get request 
 app.get('/',(req,res)=>{
-    res.send('./src/index.html')
+    res.sendFile(`${__dirname}/src/index.html`)
 })
 
 
